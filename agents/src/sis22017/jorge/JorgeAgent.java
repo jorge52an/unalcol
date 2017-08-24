cd452949b8d0cf07ba880dea89045736d10f5e61
jorge52an <jorgesolavila@gmail.com>
2017-08-24 11:32:38 -0500

    Chore: return to path

--- agents/src/sis22017/jorge/JorgeAgent.java
+++ agents/src/sis22017/jorge/JorgeAgent.java
@@ -11,6 +11,7 @@ public class JorgeAgent implements AgentProgram
 	private ArrayDeque<Byte> commands;
 	private Stack<Position> stack;
 	private HashSet<Position> visited;
+	private ArrayDeque<Position> path;
 	private Position current;
 	private Byte direction;
 
@@ -24,8 +25,10 @@ public class JorgeAgent implements AgentProgram
 		this.commands = new ArrayDeque<>();
 		this.stack = new Stack<>();
 		this.visited = new HashSet<>();
+		this.path = new ArrayDeque<>();
 		this.current = new Position( 0, 0 );
 		this.visited.add( this.current );
+		this.path.add( this.current );
 		this.direction = 0;
 	}
 
@@ -35,14 +38,6 @@ public class JorgeAgent implements AgentProgram
 			return value % divider;
 
 		return ( value % divider ) + divider;
-	}
-
-	private byte rotations( int direction, int movement )
-	{
-		if( ( direction & 1 ) == 1 )
-			return ( byte ) this.module( movement - direction, 4 );
-
-		return ( byte ) this.module( movement + direction, 4 );
 	}
 
 	private boolean[] getWalls( boolean[] walls )
@@ -55,26 +50,77 @@ public class JorgeAgent implements AgentProgram
 		return newValues;
 	}
 
+	private byte getRotations( int direction, int movement )
+	{
+		if( ( direction & 1 ) == 1 )
+			return ( byte ) this.module( movement - direction, 4 );
+
+		return ( byte ) this.module( movement + direction, 4 );
+	}
+
+	private boolean neighbor( Position current, Position goal )
+	{
+		return goal.getX() == current.getX() || goal.getY() == current.getY();
+	}
+
+	private byte getRotationsByNextPosition( Position current, Position goal, byte direction )
+	{
+		byte rotations;
+		if( goal.getX() == current.getX() )
+			if( goal.getY() == current.getY() + 1 )
+				rotations = this.getRotations( direction, 0 );
+			else
+				rotations = this.getRotations( direction, 2 );
+		else
+			if( goal.getX() == current.getX() + 1 )
+				rotations = this.getRotations( direction, 1 );
+			else
+				rotations = this.getRotations( direction, 3 );
+
+		return rotations;
+	}
+
 	private ArrayList<Byte> getActionsToGoal( Position goal )
 	{
 		ArrayList<Byte> actions = new ArrayList<>();
 
-		if( goal.getX() == this.current.getX() || goal.getY() == this.current.getY() )
+		if( neighbor( this.current, goal ) )
 		{
-			byte rotations = 0;
-			if( goal.getX() == this.current.getX() )
-				if( goal.getY() == this.current.getY() + 1 )
-					rotations = this.rotations( this.direction, 0 );
-				else
-					rotations = this.rotations( this.direction, 2 );
-			else
-				if( goal.getX() == this.current.getX() + 1 )
-					rotations = this.rotations( this.direction, 1 );
-				else
-					rotations = this.rotations( this.direction, 3 );
+			byte rotations = this.getRotationsByNextPosition( this.current, goal, this.direction );
 
 			for( int i = 0; i < rotations; i++ )
 				actions.add( ( byte ) 1 );
+			actions.add( ( byte ) 2 );
+		}
+		else
+		{
+			Position current = this.path.remove();
+			byte direction = this.direction;
+			byte rotations;
+			while( true )
+			{
+				Position next = this.path.remove();
+
+				rotations = this.getRotationsByNextPosition( current, next, direction );
+				for( int i = 0; i < rotations; i++ )
+				{
+					actions.add( ( byte ) 1 );
+					direction = ( byte ) ( ( direction + 1 ) % 4 );
+				}
+				actions.add( ( byte ) 2 );
+
+				current = next;
+				if( this.neighbor( current, goal ) )
+					break;
+			}
+
+			rotations = this.getRotationsByNextPosition( current, goal, direction );
+
+			for( int i = 0; i < rotations; i++ )
+			{
+				actions.add( ( byte ) 1 );
+				direction = ( byte ) ( ( direction + 1 ) % 4 );
+			}
 			actions.add( ( byte ) 2 );
 		}
 
@@ -122,11 +168,35 @@ public class JorgeAgent implements AgentProgram
 			}
 		}
 
-		if( !visitNewPosition )
-			return 4; //Play
+		Position goal = null;
+		ArrayList<Byte> actionsToGoal;
+		if( visitNewPosition )
+		{
+			goal = this.stack.pop();
+			actionsToGoal = this.getActionsToGoal( goal );
+		}
+		else
+		{
+			boolean allVisited = true;
+			while( true )
+			{
+				if( this.stack.empty() )
+					break;
 
-		Position goal = this.stack.pop();
-		ArrayList<Byte> actionsToGoal = this.getActionsToGoal( goal );
+				goal = this.stack.pop();
+				if( !this.visited.contains( goal ) )
+				{
+					allVisited = false;
+					break;
+				}
+			}
+
+			if( allVisited )
+				return 4; //Play
+			else
+				actionsToGoal = this.getActionsToGoal( goal );
+		}
+
 		for( int i = 1; i < actionsToGoal.size(); i++ )
 			commands.add( actionsToGoal.get( i ) );
 
@@ -179,6 +249,7 @@ public class JorgeAgent implements AgentProgram
 						break;
 				}
 				this.visited.add( this.current );
+				this.path.add( this.current );
 
 				return new Action( "advance" );
 			case 3:
